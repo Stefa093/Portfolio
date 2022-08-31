2a84cf483935e48fb514c7fb3d2eecd8d05965c8
Stefa093 <estefaniasalazar093@gmail.com>
2022-08-30 21:08:47 -0500

    final final

--- main.js
+++ main.js
@@ -4,3 +4,36 @@ const menu = document.querySelector("#menu");
 iconMenu.addEventListener("click", function () {
     menu.classList.toggle("menu__show");
 });
+
+const swiper = new Swiper('.swiper', {
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev', 
+
+},
+    breakpoints: {         
+320: {       
+slidesPerView: 3,       
+spaceBetween: 20     
+},       
+680: {       
+slidesPerView: 4,
+spaceBetween: 5     
+}, 
+loop: true,
+loopFillGroupWithBlank: true,
+
+}
+});
+const swiper1 = new Swiper ('.swiper1', {
+breakpoints: {         
+320: {       
+slidesPerView: 1,       
+spaceBetween: 30     
+},       
+680: {       
+slidesPerView: 4,
+spaceBetween: 10     
+}, 
+
+}});
